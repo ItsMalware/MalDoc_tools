eef46c8f56b0bfc0348ff9f1629c4b8781b66e08
It's Malware <Yasmine.mel.ison@hotmail.com>
2018-04-04 18:29:36 -0500

    Add files via upload

---
+++ Link_finder.vb
@@ -0,0 +1,15 @@
+Sub Link_Finder()
+Dim oShape As InlineShape, strMsg As String
+Debug.Print Application.Documents.Count
+For Each oShape In ActiveDocument.InlineShapes
+    oShape.Select
+        Debug.Print oShape.LinkFormat.SourceFullName
+        Debug.Print oShape.LinkFormat.SourcePath
+        Debug.Print oShape.LinkFormat.Type
+        oShape.Select
+        Selection.Copy
+        strMsg = strMsg & vbCrLf & oShape.LinkFormat.SourceFullName
+ 
+Next
+End Sub
+
